d96e8aeaec5d75cc59711c73a985e9a28d387b7d
Don Petersen <don@donpetersen.net>
2009-11-01 22:00:39 -0600

    Limit the number of layouts to cycle through, don't need floating or fullscreen as a tag layout.

--- rc.lua
+++ rc.lua
@@ -33,8 +33,7 @@ controlkey = "Control"
 layouts = {
   awful.layout.suit.tile, awful.layout.suit.tile.left,
   awful.layout.suit.tile.bottom, awful.layout.suit.tile.top,
-  awful.layout.suit.max.fullscreen, awful.layout.suit.magnifier,
-  awful.layout.suit.floating
+  awful.layout.suit.magnifier
 }
 -- }}}
 

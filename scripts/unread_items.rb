a8313c9b860cc76c5c6cf45572bd8c2dd2b23364
Don Petersen <don@donpetersen.net>
2009-12-29 19:06:44 -0600

    New version of RVM spits out some notification text that broke my mail script.  Also failing semi-gracefully when gems are missing.

--- scripts/unread_items.rb
+++ scripts/unread_items.rb
@@ -1,4 +1,8 @@
-require 'rubygems'
-require 'ruger'
+begin
+  require 'rubygems'
+  require 'ruger'
 
-puts Ruger.fetch_unread_counts_for_user(ARGV[0], ARGV[1])
+  puts Ruger.fetch_unread_counts_for_user(ARGV[0], ARGV[1])
+rescue LoadError
+  puts "666" # That should get my attention if the gem goes missing
+end
